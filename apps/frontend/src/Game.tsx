ef6f4bddcac490c49a87e99e1df5c8ae1667d74e
krokosik <wikkra@gmail.com>
2024-07-28 21:52:50 +0200

    Optimize messages sent

--- apps/frontend/src/Game.tsx
+++ apps/frontend/src/Game.tsx
@@ -7,6 +7,7 @@ export const Game = () => {
   const [searchParams] = useSearchParams();
   const [timeToStart, setTimeToStart] = useState<number>(5);
   const sessionId = searchParams.get("sessionId");
+  const [score, setScore] = useState<number>(0);
 
   useEffect(() => {
     if (timeToStart === 0) {
@@ -18,26 +19,29 @@ export const Game = () => {
     return () => clearTimeout(timeout);
   }, [timeToStart]);
 
-  const motionDetection = useCallback(
-    (event: DeviceMotionEvent) => {
-      const { x = 0, y = 0, z = 0 } = event.acceleration ?? {};
-      socket.emit("addScore", {
-        sessionId,
-        score: Math.sqrt(x! ** 2 + y! ** 2 + z! ** 2) / 1000,
-      });
-    },
-    [sessionId, socket]
-  );
+  const motionDetection = useCallback((event: DeviceMotionEvent) => {
+    const { x = 0, y = 0, z = 0 } = event.acceleration ?? {};
+    setScore((prev) => prev + Math.sqrt(x! ** 2 + y! ** 2 + z! ** 2) / 1000);
+  }, []);
 
   useEffect(() => {
     if (timeToStart > 0) {
       return;
     }
+    const interval = setInterval(() => {
+      socket.emit("addScore", {
+        sessionId,
+        score,
+      });
+      setScore(0);
+    }, 500);
+
     window.addEventListener("devicemotion", motionDetection, true);
     return () => {
+      clearInterval(interval);
       window.removeEventListener("devicemotion", motionDetection);
     };
-  }, [motionDetection, timeToStart]);
+  }, [motionDetection, timeToStart, sessionId, socket]);
 
   return timeToStart > 0 ? (
     <span className="countdown">
